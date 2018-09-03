7c8c9a26f69d801c4d55c6ebfb02117d5e5b0996
Johan <johan.west@sbab.se>
2018-09-03 22:56:31 +0200

    Update theme colors

--- theme.js
+++ theme.js
@@ -1,13 +1,10 @@
 module.exports = {
-  // 'primary-color': '#4477DD',
   'primary-color': '#3366CC',
   'secondary-color': '#D9E3F9',
   'surface-color': '#FFFFFF',
   'background-color': '#FFFFFF',
-  'error-color': '#B00020',
   'on-primary-color': '#FFFFFF',
-  'on-secondary-color': '#4477DD',
-  'on-surface-color': '#4477DD',
-  'on-background-color': '#4477DD',
-  'on-error-color': '#FFFFFF',
+  'on-secondary-color': '#3366CC',
+  'on-surface-color': '#3366CC',
+  'on-background-color': '#3366CC',
 };

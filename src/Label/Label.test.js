4b6e8213896fbf4ff974c98f97b0037c002ea4c1
Johan <johan.west@viablelab.com>
2018-11-05 07:01:44 +0100

    Remove components

--- src/Label/Label.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-testing-library';
-import Button from './Button';
-
-test('renders properly', () => {
-  const { container } = render(<Button>Button label</Button>);
-  expect(container.firstChild).toMatchSnapshot();
-});

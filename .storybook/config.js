aab1997b0147c0bebfe16f6c2452b40b6f6c0ecf
Johan <johan.west@viablelab.com>
2018-11-06 07:06:03 +0100

    Update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure, addDecorator } from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
+import { withOptions } from '@storybook/addon-options';
 import { withKnobs } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 import centered from '@storybook/addon-centered';
@@ -13,10 +13,11 @@ function loadStories() {
   reqStory.keys().forEach(filename => reqStory(filename));
 }
 
-setOptions({
+const options = {
   addonPanelInRight: true,
-});
+};
 
+addDecorator(withOptions(options));
 addDecorator(withKnobs);
 addDecorator(checkA11y);
 addDecorator(centered);
